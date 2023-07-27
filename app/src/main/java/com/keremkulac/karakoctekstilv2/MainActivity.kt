96b46dba3fe8e7788c5de3a2d8f71eddb0674781
keremkulac <keremkulac14@gmail.com>
2023-07-27 12:50:52 +0300

    Initial commit

---
+++ app/src/main/java/com/keremkulac/karakoctekstilv2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.keremkulac.karakoctekstilv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //dc1922020d2b5354bdc85ecf
+}
